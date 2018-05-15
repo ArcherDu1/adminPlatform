31e466ca77191f3d5e051b157f19bd408384daff
e <386953037@qq.com>
2018-05-15 15:45:20 +0800

    初始化

---
+++ src/main/java/org/archerdu/adminPlatform/AdminPlatformApplication.java
@@ -0,0 +1,12 @@
+package org.archerdu.adminPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPlatformApplication.class, args);
+	}
+}
